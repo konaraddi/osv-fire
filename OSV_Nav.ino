31d58aceb9b003d3bc1e4883e826b989cf8e35ce
omkar konaraddi <omkar.konaraddi@gmail.com>
2017-04-02 23:23:08 -0400

    added code for OSV to move towards a desired point

--- OSV_Nav.ino
+++ OSV_Nav.ino
@@ -1,4 +1,10 @@
-#include <enes100.h>
+/*TODO
+Handle constants appropriately.
+*/
+
+#include <math.h>
+
+#include <enes100.h> //I doubt we need this, but it's here just to be safe. I'll test it if we need it.
 #include <marker.h>
 #include <rf_comm.h>
 
@@ -10,6 +16,7 @@
 #include "utility/Adafruit_MS_PWMServoDriver.h"
 
 Adafruit_MotorShield AFMS = Adafruit_MotorShield();
+Adafruit_DCMotor *motor[4];
 
 SoftwareSerial mySerial(8, 9);//the ports to which the virtual RX and TX go in
 Marker marker(108); //look at QR code's back for number
@@ -17,6 +24,9 @@ RF_Comm rf(&mySerial, &marker);
 
 //assume OSV takes 1 second to rotate its wheels once
 float timeToRotateOnce= 1000;
+
+//circumference of our OSV's wheels (in meters)
+float circumferenceOfOSVWheel= 2;
 
 //OSV's CURRENT COORDINATES
 float currentX;
@@ -34,14 +44,14 @@ float By= 1.675;
 void setup(){
     Serial.begin(9600);
     AFMS.begin();
+
+    //obtain OSV's landing coordinates
+    //updateCurrentLocation();
 }
 
 void loop(){
-
-    moveStraight(50, 50, 'F');
     //TODO Optimize exiting the wall after the basics work
     //EXIT THE WALL THROUGH POINT A (FOR NOW, WILL OPTIMIZE LATER)
-
 
 
     //TRAVEL TOWARDS FIRE SITE
@@ -58,26 +68,31 @@ void loop(){
 }
 
 void updateCurrentLocation(){
+
     if(rf.updateLocation()){
         currentX= marker.x;
         currentY= marker.y;
         currentTheta= marker.theta;
+        rf.println("location updated");
     }else{
         //this shouldn't be happening....
         rf.println("oh lawrd");
         rf.println("updateCurrentLocation broke");
     }
+
 }
 
 /*
 The functions below use speed (PWM: 0-255) and duration. For now, the duration
 is time-based (milliseconds). It will be changed to rotations in the future.
 The direction should be FORWARD or BACKWARD.
+
+TESTED thru Serial print
 */
 void moveStraight(int speed, int duration, char direction){
+
     //motors on the left are motor[0] and motor [1] (top view of OSV with front facing North)
     //motors on the right are motor[2] and motor [3] (top view of OSV with front facing North)
-    Adafruit_DCMotor *motor[4];
     for(int i= 0; i < 4; i++){
         motor[i]= AFMS.getMotor(i + 1);
         motor[i]->setSpeed(speed);
@@ -87,6 +102,8 @@ void moveStraight(int speed, int duration, char direction){
     The if-statement has been kept outside of the loops intentionally. While this
     does take up more space, this is far more efficient than keeping the if-statements
     inside one loop.
+
+    The following statements set the motors to the specified direction (FORWARD or BACKWARD).
     */
     if(direction == 'F'){
         for(int i= 0; i < 4; i++){
@@ -116,4 +133,44 @@ void moveStraight(int speed, int duration, char direction){
     Serial.println(duration);
     Serial.println(direction);
     Serial.print("ms");
+
+}
+
+/*
+NEEDS TO BE TESTED THOROUGHLY.
+*/
+void moveTowardsPoint(float desiredX, float desiredY){
+
+    updateCurrentLocation();
+
+    float Ymovement= desiredY - currentY;
+    float Xmovement= desiredX - currentX;
+
+    //the direction the OSV needs to face
+    //returns a range of -pi to +pi
+    float theta= atan2(Ymovement, Xmovement);
+
+    if(theta < 0){
+        //OSV will turn clockwise
+        turnClockWise(theta);
+    }else{
+        //OSV will turn counterclockwise
+        turnCounterClockWise(theta);
+    }
+
+    float distanceToMove= sqrt(pow(Ymovement,2) + pow(Xmovement, 2));
+    moveStraight(50, (distanceToMove / circumferenceOfOSVWheel) * timeToRotateOnce, 'F');
+
+    updateCurrentLocation();
+    if(currentX > desiredX + 0.1 || currentX < desiredX - 0.1 || currentY > desiredY + 0.1 || currentY < desiredY - 0.1{
+        moveTowardsPoint(desiredX, desiredY);
+    }
+    
+}
+
+void turnClockWise(float radiansToTurn){
+
+}
+void turnCounterClockWise(float radiansToTurn){
+
 }
--- README.md
+++ README.md
@@ -11,17 +11,17 @@ libraries before uploading:
 ## FAQs
 
 #### 1. Why hasn't Omkar made any recent commits?
-
-I may be learning how to implement some functionality (e.g. learning C/C++,
-    using RF with tanks). My practice files are not pushed to this repository.
-    <b>This repository is solely for code that should run on the OSV.</b>
+I may be learning how to implement some functionality (e.g. learning C/C++).
+My practice files are not pushed to this repository. <b>This repository is
+solely for code that should run on the OSV.</b>
 
 #### 2. Why is there so little code? Is this all Omkar has done?
 Please see the answer to [Question #1](https://github.com/konaraddio/OSV_Nav#1-why-hasnt-omkar-made-any-recent-commits).
+And most of coding is thinking.
 
 #### 3. How do I run this code?
 Download this using the green "Clone or download" button. Add the necessary
-libraries to your /Arduino/library folder. Upload to OSV.
+libraries to your /Arduino/library/ folder. Upload to OSV.
 
 #### 4. Hmmm, something's not working. What should I do?
 Message me (Omkar).
