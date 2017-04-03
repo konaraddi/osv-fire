55ae7dc4c35f7e8c26b4b305cdce93bc9b5472cb
omkar konaraddi <omkar.konaraddi@gmail.com>
2017-04-02 21:32:28 -0400

    methods for updating location and moving straight

--- OSV_Nav.ino
+++ OSV_Nav.ino
@@ -5,39 +5,20 @@
 #include <SoftwareSerial.h>
 #include <Arduino.h>
 
-//did i put the Adafruit library in the right place? Should the .h file be taken out of the Adafruit folder
 #include <Wire.h>
 #include <Adafruit_MotorShield.h>
 #include "utility/Adafruit_MS_PWMServoDriver.h"
 
+float currentX;
+float currentY;
+float currentTheta;
 
-//The below is the MAIN thread
+SoftwareSerial mySerial(8, 9);//the ports to which the virtual RX and TX go in
+Marker marker(108); //look at QR code's back for number
+RF_Comm rf(&mySerial, &marker);
+
 void setup(){
-    //TO GET THE ARDUINO TO MOVE
-    Adafruit_MotorShield AFMS = Adafruit_MotorShield();
-    /*
-    Check ports and change if necessary before running!
-    These ports are the M ports
-    */
 
-    //TODO
-    //make the motors an array and provide methods for the real deal
-    Adafruit_DCMotor *motor1 = AFMS.getMotor(1);
-    //motor1->setSpeed(50); //0-255
-    Adafruit_DCMotor *motor2 = AFMS.getMotor(2);
-    //motor2->setSpeed(50);
-    Adafruit_DCMotor *motor3 = AFMS.getMotor(3);
-    //motor3->setSpeed(50);
-    Adafruit_DCMotor *motor4 = AFMS.getMotor(4);
-    //motor4->setSpeed(50);
-
-    /*makes the OSV revolve on a point, unless the include tags failed (or library incorrectly imported)
-    OR, if the OSV moved forward, the motors' wires need to be swapped in a place or more
-    */
-    motor1->run(FORWARD);
-    motor2->run(FORWARD);
-    motor3->run(BACKWARD);
-    motor4->run(BACKWARD);
 }
 
 void loop(){
@@ -45,9 +26,57 @@ void loop(){
 
 
     /*
-    The statement below will run forever so
-    this current loop() will never finish running.
-    The purpose of this is for the loop() to never repeat.
+    The statement below will run forever so this current loop() will never
+    finish running. The purpose of this is for the loop() to never repeat.
     */
     while(1);
+}
+
+void updateCurrentLocation(){
+    if(rf.updateLocation()){
+        currentX= marker.x;
+        currentY= marker.y;
+        currentTheta= marker.theta;
+    }else{
+        rf.println("oh lawrd");
+        rf.println("updateCurrentLocation broke");
+    }
+}
+
+/*
+The functions below use speed (PWM: 0-255) and duration. For now, the duration
+is time-based. It will be changed to rotations in the future. The direction
+indicates FORWARD or BACKWARD.
+*/
+void moveStraight(int speed, int duration, char direction){
+    Adafruit_MotorShield AFMS = Adafruit_MotorShield();
+    AFMS.begin();
+    Adafruit_DCMotor *motor1 = AFMS.getMotor(1);
+    Adafruit_DCMotor *motor2 = AFMS.getMotor(2);
+    Adafruit_DCMotor *motor3 = AFMS.getMotor(3);
+    Adafruit_DCMotor *motor4 = AFMS.getMotor(4);
+
+    motor1->setSpeed(speed);
+    motor2->setSpeed(speed);
+    motor3->setSpeed(speed);
+    motor4->setSpeed(speed);
+
+    if(direction == 'FORWARD'){
+        motor1->run(FORWARD);
+        motor2->run(FORWARD);
+        motor3->run(FORWARD);
+        motor4->run(FORWARD);
+    }else if(direction == 'BACKWARD'){
+        motor1->run(BACKWARD);
+        motor2->run(BACKWARD);
+        motor3->run(BACKWARD);
+        motor4->run(BACKWARD);
+    }
+
+    delay(duration);
+
+    motor1->run(RELEASE);
+    motor2->run(RELEASE);
+    motor3->run(RELEASE);
+    motor4->run(RELEASE);
 }
