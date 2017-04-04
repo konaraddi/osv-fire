b8c073c44b8f6ac3c9d1487aaf363f3fd866208f
omkar konaraddi <omkar.konaraddi@gmail.com>
2017-04-03 21:58:14 -0400

    fixed typo

--- Test_Foreward_Backward/Test_Foreward_Backward.ino
+++ Test_Foreward_Backward/Test_Foreward_Backward.ino
@@ -21,20 +21,15 @@ void loop() {
 
   motor1->run(FORWARD);
   motor2->run(FORWARD);
-  motor3->run(BACKWARD);
-  motor4->run(BACKWARD);
+  motor3->run(FORWARD);
+  motor4->run(FORWARD);
 
   delay(5000);
 
   motor1->run(BACKWARD);
   motor2->run(BACKWARD);
-  motor3->run(FORWARD);
-  motor4->run(FORWARD);
+  motor3->run(BACKWARD);
+  motor4->run(BACKWARD);
 
   delay(5000);
 }
-
-
-
-
-
